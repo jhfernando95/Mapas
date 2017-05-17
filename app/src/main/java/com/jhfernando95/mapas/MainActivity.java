c9b43e64c6f50cb1504a5537acd174a71a8beaf1
jhfernando <jhfernando.95@gmail.com>
2017-05-17 16:50:06 -0500

    17/052017

--- app/src/main/java/com/jhfernando95/mapas/MainActivity.java
+++ app/src/main/java/com/jhfernando95/mapas/MainActivity.java
@@ -18,6 +18,36 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     GoogleMap m_map;
     boolean mapReady = false;
 
+    static final  CameraPosition PASTO =CameraPosition.builder()
+            .target(new LatLng(1.2086284 , -77.2779443))
+            .zoom(15)
+            .bearing(90)
+            .tilt(45)
+            .build();
+
+    static final  CameraPosition SEATTLE =CameraPosition.builder()
+            .target(new LatLng(1.2086284 , -77.2779443))
+            .zoom(15)
+            .bearing(90)
+            .tilt(45)
+            .build();
+
+    static final  CameraPosition DUBLIN =CameraPosition.builder()
+            .target(new LatLng(1.2086284 , -77.2779443))
+            .zoom(15)
+            .bearing(90)
+            .tilt(45)
+            .build();
+
+    static final  CameraPosition TOKYO =CameraPosition.builder()
+            .target(new LatLng(1.2086284 , -77.2779443))
+            .zoom(15)
+            .bearing(90)
+            .tilt(45)
+            .build();
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,28 +55,31 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
 
 
-        Button btnNormal = (Button) findViewById(R.id.id_btn_normal);
+        Button btnNormal = (Button) findViewById(R.id.id_btn_seattle);
         btnNormal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                m_map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                motoclima(SEATTLE);
+               // m_map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
             }
         });
 
 
-        Button btnSatelite = (Button) findViewById(R.id.id_btn_satelite);
+        Button btnSatelite = (Button) findViewById(R.id.id_btn_dublin);
         btnSatelite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                motoclima(DUBLIN);
+               // m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
             }
         });
 
-        Button btnHibrido = (Button) findViewById(R.id.id_btn_hibrido);
+        Button btnHibrido = (Button) findViewById(R.id.id_btn_tokyo);
         btnHibrido.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                m_map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                motoclima(TOKYO);
+                //m_map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
             }
         });
 
@@ -61,9 +94,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
 
         mapReady = true;
-        LatLng pasto = new LatLng(1.2086284 , -77.2779443);
-        CameraPosition target = CameraPosition.builder().target(pasto).zoom(15).build();
-        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+        m_map = googleMap;
+        //LatLng pasto = new LatLng(1.2086284 , -77.2779443);
+        //CameraPosition target = CameraPosition.builder().target(pasto).zoom(15).build();
+        //m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+        motoclima(PASTO);
 
     }
+    public  void motoclima(CameraPosition perro) {
+
+        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(perro), 2000, null);
+
+    }
+
+
 }
